9424b0f004772a879fb48a111d5b222439d5f7b4
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-02-25 15:43:43 +0630

    add download boxes

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,17 @@ module.exports = {
     },
     extend: {
       colors: {
-        softBlue: 'hsl(231, 69%, 60%)',
-        softRed: 'hsl(0, 94%, 66%)',
-        grayishBlue: 'hsl(229, 8%, 60%)',
-        veryDarkBlue: 'hsl(229, 31%, 21%)',
+        softBlue: "hsl(231, 69%, 60%)",
+        softRed: "hsl(0, 94%, 66%)",
+        grayishBlue: "hsl(229, 8%, 60%)",
+        veryDarkBlue: "hsl(229, 31%, 21%)",
       },
       fontFamily: {
-        sans: ['Rubik', 'sans-serif']
+        sans: ["Rubik", "sans-serif"],
       },
       backgroundImage: () => ({
-        
-      })
+        dots: "url('../images/bg-dots.svg')",
+      }),
     },
   },
   plugins: [],
