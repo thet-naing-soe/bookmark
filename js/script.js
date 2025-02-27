5253af61feb2d54cfbe7781b6da3b1bcc1de4562
thet-naing-soe <mehnthetnaingsoe@gmail.com>
2025-02-27 14:43:29 +0630

    mobile menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 const tabs = document.querySelectorAll(".tab");
 const panels = document.querySelectorAll(".panel");
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+const logo = document.getElementById("logo");
 
 // Tab menu event listener
 tabs.forEach((tab) => tab.addEventListener("click", onTabClick));
+
+// Hamburger Button Listener
+btn.addEventListener("click", navToggle);
 
 function onTabClick(e) {
   // Deactivate all tabs
@@ -20,4 +26,16 @@ function onTabClick(e) {
     .getElementById("panels")
     .getElementsByClassName(classString)[0]
     .classList.remove("hidden");
+}
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex")
+  menu.classList.toggle("hidden")
+
+  if(menu.classList.contains("flex")) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg')
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg')
+  }
 }
